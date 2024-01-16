44b2388e2cdc3b903e1383cfe06c268ac8d75002
David MacPherson <david@tensorworks.com.au>
2024-01-16 13:13:30 +1000

    Added timeout to the udp dial

--- client/main.go
+++ client/main.go
@@ -5,19 +5,21 @@ import (
 	"flag"
 	"fmt"
 	"net"
+	"time"
 )
 
 func main() {
 
 	port := flag.Int("port", 30000, "Port to send udp on")
 	ip := flag.String("ip", "127.0.0.1", "IP to send on")
+	timeoutFlag := flag.Int("timeout", 2, "Timeout to dial the udp server")
 
 	flag.Parse()
 
 	fmt.Printf("Sending address: %s:%v\n", *ip, *port)
 
 	p := make([]byte, 2048)
-	conn, err := net.Dial("udp", fmt.Sprintf("%s:%v", *ip, *port))
+	conn, err := net.DialTimeout("udp", fmt.Sprintf("%s:%v", *ip, *port), time.Duration(*timeoutFlag)*time.Second)
 	if err != nil {
 		fmt.Printf("Some error %v", err)
 		return
