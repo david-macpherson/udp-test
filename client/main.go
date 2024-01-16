3e307548f4eccbbf5e166aadd402fff3315f416c
David MacPherson <david@tensorworks.com.au>
2024-01-16 13:18:31 +1000

    Add better logging

--- client/main.go
+++ client/main.go
@@ -10,16 +10,22 @@ import (
 
 func main() {
 
-	port := flag.Int("port", 30000, "Port to send udp on")
-	ip := flag.String("ip", "127.0.0.1", "IP to send on")
+	portFlag := flag.Int("port", 30000, "Port to send udp on")
+	ipFlag := flag.String("ip", "127.0.0.1", "IP to send on")
 	timeoutFlag := flag.Int("timeout", 2, "Timeout to dial the udp server")
 
 	flag.Parse()
 
-	fmt.Printf("Sending address: %s:%v\n", *ip, *port)
+	ip := *ipFlag
+	port := *portFlag
+	timeout := time.Duration(*timeoutFlag) * time.Second
+
+	fmt.Printf("IP:     %s\n", ip)
+	fmt.Printf("Port:   %v\n", port)
+	fmt.Printf("Timeout %v\n", timeout)
 
 	p := make([]byte, 2048)
-	conn, err := net.DialTimeout("udp", fmt.Sprintf("%s:%v", *ip, *port), time.Duration(*timeoutFlag)*time.Second)
+	conn, err := net.DialTimeout("udp", fmt.Sprintf("%s:%v", ip, port), time.Duration(*timeoutFlag)*time.Second)
 	if err != nil {
 		fmt.Printf("Some error %v", err)
 		return
